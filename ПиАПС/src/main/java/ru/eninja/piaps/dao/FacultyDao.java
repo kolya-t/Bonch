12f11829d16b9559c4591b749fef31f2f2232a8f
kolya-t <toporkov.kolya96@gmail.com>
2017-04-17 13:47:16 +0300

    add faculty table

--- ПиАПС/src/main/java/ru/eninja/piaps/dao/FacultyDao.java
+++ ПиАПС/src/main/java/ru/eninja/piaps/dao/FacultyDao.java
@@ -1,8 +1,9 @@
 package ru.eninja.piaps.dao;
 
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import ru.eninja.piaps.domain.Faculty;
 
 
-public interface FacultyDao extends PagingAndSortingRepository<Faculty, Integer> {
+public interface FacultyDao extends PagingAndSortingRepository<Faculty, Integer>, JpaSpecificationExecutor<Faculty> {
 }
--- ПиАПС/src/main/java/ru/eninja/piaps/web/FacultyController.java
+++ ПиАПС/src/main/java/ru/eninja/piaps/web/FacultyController.java
@@ -3,16 +3,29 @@ package ru.eninja.piaps.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.eninja.piaps.dao.FacultyDao;
 import ru.eninja.piaps.domain.Faculty;
+import ru.eninja.piaps.util.specifications.Filter;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 
 @Controller
 @RequestMapping("/faculties")
 public class FacultyController {
+
+    public static final Map<String, String> FIELD_TO_NAME_MAP = new LinkedHashMap<String, String>() {{
+        put("id", "ID");
+        put("name", "Название");
+    }};
 
     private final FacultyDao facultyDao;
 
@@ -21,10 +34,35 @@ public class FacultyController {
         this.facultyDao = facultyDao;
     }
 
+    @SuppressWarnings("unchecked")
+    @RequestMapping(method = RequestMethod.GET, params = {"filterFields", "filterWord"})
+    public String showFacultiesTable(Model model,
+                                 @SortDefault("id") Pageable pageable,
+                                 @RequestParam List<String> filterFields,
+                                 @RequestParam String filterWord) {
+        if (filterWord.isEmpty()) {
+            return showFacultiesTable(model, pageable);
+        }
+
+        Page page = facultyDao.findAll(new Filter(filterFields, filterWord), pageable);
+        model.addAttribute("page", page);
+        model.addAttribute("filterFields", filterFields);
+        model.addAttribute("filterWord", filterWord);
+
+        return "faculties/table";
+    }
+
     @RequestMapping(method = RequestMethod.GET)
-    public String getAllStudents(Model model, Pageable pageable) {
+    public String showFacultiesTable(Model model, @SortDefault("id") Pageable pageable) {
+        Page<Faculty> page = facultyDao.findAll(pageable);
+        model.addAttribute("page", page);
+        return "faculties/table";
+    }
+
+    @RequestMapping(value = "/tree", method = RequestMethod.GET)
+    public String showFacultiesTree(Model model, Pageable pageable) {
         Page<Faculty> page = facultyDao.findAll(pageable);
         model.addAttribute("faculties", page);
-        return "faculties/list";
+        return "faculties/tree";
     }
 }
