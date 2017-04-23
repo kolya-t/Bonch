bd647cbcc654e9f67454b05ef878d296f8a3ad54
kolya-t <toporkov.kolya96@gmail.com>
2017-04-23 22:13:33 +0300

    new domain classes structure. added GeneratedValueEntity and NonGeneratedValueEntity

--- ПиАПС/src/main/java/ru/eninja/piaps/dao/GroupDao.java
+++ ПиАПС/src/main/java/ru/eninja/piaps/dao/GroupDao.java
@@ -2,7 +2,7 @@ package ru.eninja.piaps.dao;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.eninja.piaps.domain.Group;
+import ru.eninja.piaps.domain.impl.Group;
 
 
 public interface GroupDao extends PagingAndSortingRepository<Group, String>, JpaSpecificationExecutor<Group> {
--- ПиАПС/src/main/java/ru/eninja/piaps/dao/SpecialtyDao.java
+++ ПиАПС/src/main/java/ru/eninja/piaps/dao/SpecialtyDao.java
@@ -2,7 +2,7 @@ package ru.eninja.piaps.dao;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
-import ru.eninja.piaps.domain.Specialty;
+import ru.eninja.piaps.domain.impl.Specialty;
 
 
 public interface SpecialtyDao extends PagingAndSortingRepository<Specialty, String>, JpaSpecificationExecutor<SpecialtyDao> {
